48f48ebfad3ee88a377f3560f08f7523f4403477
Maciej Myśliński <maciej.myslinski@netguru.co>
2018-03-10 23:28:02 +0100

    add react-native-google-signin

--- android/app/src/main/java/com/fixit/MainApplication.java
+++ android/app/src/main/java/com/fixit/MainApplication.java
@@ -3,6 +3,7 @@ package com.fixit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNGoogleSigninPackage(),
           new RNFirebasePackage(),
           new RNFirebaseAuthPackage(),
           new RNFirebaseFirestorePackage()
