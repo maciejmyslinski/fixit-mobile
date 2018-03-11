089e4e1147bb9d3b3a3909745c78e22044e605e0
Maciej Myśliński <maciej.myslinski@netguru.co>
2018-03-11 21:20:09 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   plugins: ['react', 'react-native'],
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react-native/all'
-  ]
+  extends: ['plugin:react/recommended', 'plugin:react-native/all'],
+  rules: {
+    'react-native/no-color-literals': 'off',
+  }
 };
