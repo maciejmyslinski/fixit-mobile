4661192bfd3d7104bb2b4e84bad347a2e6e5c32f
Maciej Myśliński <maciej.myslinski@netguru.co>
2018-03-11 20:55:32 +0100

    Add login button

--- App.js
+++ App.js
@@ -8,11 +8,14 @@ import {
   ListItem,
   Text
 } from 'native-base';
+import { View, StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import firebase from 'react-native-firebase';
+import { GoogleSigninButton } from 'react-native-google-signin';
 import { JobsProvider } from 'src/providers/JobsProvider';
 import { AuthProvider } from 'src/providers/AuthProvider';
 import { Navigator } from 'src/components/Navigator';
+import { login } from 'src/utils/auth';
 
 class App extends Component {
   render() {
@@ -25,11 +28,34 @@ class App extends Component {
                 render={jobs => <Navigator screenProps={{ jobs, auth }} />}
               />
             );
-          return null;
+          console.log('I am here');
+          return (
+            <View style={styles.loginWrapper}>
+              <Text>Zaloguj</Text>
+              <GoogleSigninButton
+                style={styles.loginButton}
+                color={GoogleSigninButton.Color.Light}
+                size={GoogleSigninButton.Size.Icon}
+                onPress={login}
+              />
+            </View>
+          );
         }}
       />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  loginWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  loginButton: {
+    width: 120,
+    height: 44
+  }
+});
 
 export default App;
